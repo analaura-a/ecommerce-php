15f4d1de585fb78123e589773d8e0b02110cd13f
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2023-07-03 21:00:27 -0300

    Cuando se borra una prenda, se borran tambien sus talles secundarios

--- admin/actions/delete_prenda_action.php
+++ admin/actions/delete_prenda_action.php
@@ -6,6 +6,7 @@ $id = $_GET['id'] ?? FALSE;
 $prenda = (new Prenda())->catalogo_por_id($id);
 
 try {
+    (new Prenda())->clear_talles_sec($id);
     $prenda->delete();
     header("Location: ../index.php?seccion=admin_prendas");
 } catch (\Exception $e) {

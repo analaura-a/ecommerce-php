7f20d827af164e34312825197da57252ffee7857
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2023-07-03 22:02:42 -0300

    al eliminar una prenda, se elimina su foto localmente también

--- admin/actions/delete_prenda_action.php
+++ admin/actions/delete_prenda_action.php
@@ -7,6 +7,9 @@ $prenda = (new Prenda())->catalogo_por_id($id);
 
 try {
     (new Prenda())->clear_talles_sec($id);
+    if (!empty($prenda->getImagen())) {
+        (new Imagen())->borrarImagen(__DIR__ . "/../../img/productos/" . $prenda->getImagen());
+    }
     $prenda->delete();
     header("Location: ../index.php?seccion=admin_prendas");
 } catch (\Exception $e) {
